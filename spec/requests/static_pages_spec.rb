9612db026103654b622bf40f8199aef7a3cbec54
Matt Currier <fingerskier@gmail.com>
2013-11-04 19:38:55 -0700

    Added auto-test gems

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -26,14 +26,26 @@ describe "Static Pages" do
   end
 
   describe "About page" do
-  	it "should have the content 'About Us'" do
-  		visit '/static_pages/about'
-  		expect(page).to have_content('About Us')
-  	end
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About Us')
+    end
 
     it "should have the right title" do
       visit '/static_pages/about'
       expect(page).to have_title("Ruby on Rails Tutorial Sample App | About")
+     end
+  end
+
+  describe "Contact page" do
+    it "should have the content 'Contact Us'" do
+      visit '/static_pages/contact'
+      expect(page).to have_content('Contact Us')
+    end
+
+    it "should have the right title" do
+      visit '/static_pages/contact'
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
      end
   end
 end
